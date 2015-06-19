366a564823f7ddd658f8ac156f7118365f9ed9e6
ramanp <ramanp@email.chop.edu>
2015-06-19 13:50:58 -0400

    added driver code

---
+++ code/driver.R
@@ -0,0 +1,35 @@
+##########################################
+#Objective : Compare various methods of dealing with
+#continuous variables during a survival analysis. 
+#
+##########################################
+
+
+##########################################
+#Source all code here
+#1. Methods of survival analysis
+source("KaplanScan.R");
+#2. Methods of comparing surival analysis
+##########################################
+
+
+##########################################
+#Read in data
+annot_nbv <- read.delim("../data/Neuroblastoma_Versteeg/annot.txt")
+exprs_nbv <- read.delim("../data/Neuroblastoma_Versteeg/exprs.txt")
+##########################################
+
+
+
+##########################################
+#Run Surival analysis using various techniques
+
+
+
+##########################################
+
+
+
+
+
+
