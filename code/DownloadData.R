6c9207191a96c651d6a0a56ec6010e82525b31e5
PichaiRaman <pichai.raman@gmail.com>
2015-09-05 14:53:48 -0400

    Update DownloadData.R

--- code/DownloadData.R
+++ code/DownloadData.R
@@ -36,14 +36,20 @@ DownloadGSE <- function(gseName=null, myWD=".")
         check="Success";
     }
     print(paste("There are ", numFiles, " files downloaded and ", numSamples, " samples in your annotation file so your download was a ", check, sep=""));
-    
-    
 }
 
 
+DownloadGSE("GSE425",myWD="../data/");
 
+DownloadGSE("GSE1159",myWD="../data/");
 
+DownloadGSE("GSE14468",myWD="../data/");
 
+DownloadGSE("GSE30258",myWD="../data/");
+
+DownloadGSE("GSE30285",myWD="../data/");
+
+DownloadGSE("GSE37642",myWD="../data/");
 
 
 
