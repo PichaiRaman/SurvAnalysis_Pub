ca88697fdd47a6a881bb100d4b6cc16080e72cf2
PichaiRaman <pichai.raman@gmail.com>
2015-09-19 08:56:52 -0400

    Create gmm.R

---
+++ code/gmm.R
@@ -0,0 +1,30 @@
+
+#This will yield a p-value and show a plot
+gmmSA <- function(genes, myData, createPlot=T, tVar="time", eVar="event")
+{
+    #Get metadata
+    
+    tmpMeta <- myData[[2]];
+    myGene <- myData[[1]][genes,];
+    tmpMeta[,"Gene"] <- as.numeric(myGene);
+
+    tmpMix <- normalmixEM(tmpMeta[,"Gene"], maxit=1000, maxrestarts=1000);
+    myOut <- data.frame(tmpMix$posterior);
+    tmpMeta[,"Groups"] <- myOut[,1]> myOut[,2];
+    tmpMeta <- tmpMeta[order(tmpMeta[,"Gene"]),]
+  
+    
+    
+
+    #time
+    timeVar <- tmpMeta[,tVar];
+    #event
+    eventVar <- tmpMeta[,eVar];
+    
+    #createsurvival
+    t.Surv <- Surv(timeVar, eventVar);
+    t.survfit <- survfit(t.Surv~Groups, data= tmpMeta);
+    myP <- pchisq(survdiff(t.Surv~ Groups, data= tmpMeta, rho=0)$chisq, df=1, lower=F);
+    out <- c(genes, myP)   
+    
+}
