2a4f1692bc1354d38ec834074c0264896b091733
PichaiRaman <pichai.raman@gmail.com>
2015-09-19 13:22:52 -0400

    Update driver.R

--- code/driver.R
+++ code/driver.R
@@ -11,6 +11,7 @@ library("ggplot2");
 library("lattice");
 library("pheatmap");
 library("mixtools");
+library("snow");
 
 
 ##########################################
@@ -64,96 +65,98 @@ geneCV <- sort(geneCV, T);
 #do all genes but kmScan takes a bit of time so for testing purposes let's set n to a small number
 #
 numGenes <- 20531;
+clus <- makeCluster(50);
 
 #KM Scan Technique
-kmScan_ov <- sapply(names(geneCV)[1:numGenes], FUN=kapmPlot, ov, F, tVar="TimeVar", eVar="eventVar");
+kmScan_ov <- parSapply(clus, names(geneCV)[1:numGenes], FUN=kapmPlot, ov, F, tVar="TimeVar", eVar="eventVar");
 kmScan_ov <- data.frame(t(data.frame(kmScan_ov)));
 colnames(kmScan_ov) <- c("Gene", "P.Value", "Adj.P.Value");
 
-kmScan_pr <- sapply(names(geneCV)[1:numGenes], FUN=kapmPlot, pr, F, tVar="TimeVar", eVar="eventVar");
+kmScan_pr <- parSapply(clus, names(geneCV)[1:numGenes], FUN=kapmPlot, pr, F, tVar="TimeVar", eVar="eventVar");
 kmScan_pr <- data.frame(t(data.frame(kmScan_pr)));
 colnames(kmScan_pr) <- c("Gene", "P.Value", "Adj.P.Value");
 
-kmScan_ki <- sapply(names(geneCV)[1:numGenes], FUN=kapmPlot, ki, F, tVar="TimeVar", eVar="eventVar");
+kmScan_ki <- parSapply(clus, names(geneCV)[1:numGenes], FUN=kapmPlot, ki, F, tVar="TimeVar", eVar="eventVar");
 kmScan_ki <- data.frame(t(data.frame(kmScan_ki)));
 colnames(kmScan_ov) <- c("Gene", "P.Value", "Adj.P.Value");
 
-kmScan_hn <- sapply(names(geneCV)[1:numGenes], FUN=kapmPlot, hn, F, tVar="TimeVar", eVar="eventVar");
+kmScan_hn <- parSapply(clus, names(geneCV)[1:numGenes], FUN=kapmPlot, hn, F, tVar="TimeVar", eVar="eventVar");
 kmScan_hn <- data.frame(t(data.frame(kmScan_hn)));
 colnames(kmScan_hn) <- c("Gene", "P.Value", "Adj.P.Value");
 
 
 #Quantile Technique, cutting at median
-qCut50_ov <- sapply(names(geneCV)[1:numGenes], FUN=quantCutSA, ov, F, quantLow=.50,  quantHigh=.50, tVar="TimeVar", eVar="eventVar");
+qCut50_ov <- parSapply(clus, names(geneCV)[1:numGenes], FUN=quantCutSA, ov, F, quantLow=.50,  quantHigh=.50, tVar="TimeVar", eVar="eventVar");
 qCut50_ov <- data.frame(t(data.frame(qCut50_ov)));
 colnames(qCut50_ov) <- c("Gene", "P.Value");
 
-qCut50_pr <- sapply(names(geneCV)[1:numGenes], FUN=quantCutSA, pr, F, quantLow=.50,  quantHigh=.50, tVar="TimeVar", eVar="eventVar");
+qCut50_pr <- parSapply(clus, names(geneCV)[1:numGenes], FUN=quantCutSA, pr, F, quantLow=.50,  quantHigh=.50, tVar="TimeVar", eVar="eventVar");
 qCut50_pr <- data.frame(t(data.frame(qCut50_pr)));
 colnames(qCut50_pr) <- c("Gene", "P.Value");
 
-qCut50_ki <- sapply(names(geneCV)[1:numGenes], FUN=quantCutSA, ki, F, quantLow=.50,  quantHigh=.50, tVar="TimeVar", eVar="eventVar");
+qCut50_ki <- parSapply(clus, names(geneCV)[1:numGenes], FUN=quantCutSA, ki, F, quantLow=.50,  quantHigh=.50, tVar="TimeVar", eVar="eventVar");
 qCut50_ki <- data.frame(t(data.frame(qCut50_ki)));
 colnames(qCut50_ki) <- c("Gene", "P.Value");
 
-qCut50_hn <- sapply(names(geneCV)[1:numGenes], FUN=quantCutSA, hn, F, quantLow=.50,  quantHigh=.50, tVar="TimeVar", eVar="eventVar");
+qCut50_hn <- parSapply(clus, names(geneCV)[1:numGenes], FUN=quantCutSA, hn, F, quantLow=.50,  quantHigh=.50, tVar="TimeVar", eVar="eventVar");
 qCut50_hn <- data.frame(t(data.frame(qCut50_hn)));
 colnames(qCut50_hn) <- c("Gene", "P.Value");
 
 
 #Quantile Technique, cutting at 25 and 75
-qCut2575_ov <- sapply(names(geneCV)[1:numGenes], FUN=quantCutSA, ov, F, quantLow=.25,  quantHigh=.75, tVar="TimeVar", eVar="eventVar");
+qCut2575_ov <- parSapply(clus, names(geneCV)[1:numGenes], FUN=quantCutSA, ov, F, quantLow=.25,  quantHigh=.75, tVar="TimeVar", eVar="eventVar");
 qCut2575_ov <- data.frame(t(data.frame(qCut2575_ov)));
 colnames(qCut2575_ov) <- c("Gene", "P.Value");
 
-qCut2575_pr <- sapply(names(geneCV)[1:numGenes], FUN=quantCutSA, pr, F, quantLow=.25,  quantHigh=.75, tVar="TimeVar", eVar="eventVar");
+qCut2575_pr <- parSapply(clus, names(geneCV)[1:numGenes], FUN=quantCutSA, pr, F, quantLow=.25,  quantHigh=.75, tVar="TimeVar", eVar="eventVar");
 qCut2575_pr <- data.frame(t(data.frame(qCut2575_pr)));
 colnames(qCut2575_pr) <- c("Gene", "P.Value");
 
-qCut2575_ki <- sapply(names(geneCV)[1:numGenes], FUN=quantCutSA, ki, F, quantLow=.25,  quantHigh=.75, tVar="TimeVar", eVar="eventVar");
+qCut2575_ki <- parSapply(clus, names(geneCV)[1:numGenes], FUN=quantCutSA, ki, F, quantLow=.25,  quantHigh=.75, tVar="TimeVar", eVar="eventVar");
 qCut2575_ki <- data.frame(t(data.frame(qCut2575_ki)));
 colnames(qCut2575_ki) <- c("Gene", "P.Value");
 
-qCut2575_hn <- sapply(names(geneCV)[1:numGenes], FUN=quantCutSA, hn, F, quantLow=.25,  quantHigh=.75, tVar="TimeVar", eVar="eventVar");
+qCut2575_hn <- parSapply(clus, names(geneCV)[1:numGenes], FUN=quantCutSA, hn, F, quantLow=.25,  quantHigh=.75, tVar="TimeVar", eVar="eventVar");
 qCut2575_hn <- data.frame(t(data.frame(qCut2575_hn)));
 colnames(qCut2575_hn) <- c("Gene", "P.Value");
 
 #Cox Regression
-coxReg_ov <- sapply(names(geneCV)[1:numGenes], FUN= coxReg, ov);
+coxReg_ov <- parSapply(clus, names(geneCV)[1:numGenes], FUN= coxReg, ov);
 coxReg_ov <- data.frame(t(data.frame(coxReg_ov)));
 colnames(coxReg_ov) <- c("Gene", "P.Value");
 
-coxReg_pr <- sapply(names(geneCV)[1:numGenes], FUN= coxReg, pr);
+coxReg_pr <- parSapply(clus, names(geneCV)[1:numGenes], FUN= coxReg, pr);
 coxReg_pr <- data.frame(t(data.frame(coxReg_pr)));
 colnames(coxReg_pr) <- c("Gene", "P.Value");
 
-coxReg_ki <- sapply(names(geneCV)[1:numGenes], FUN= coxReg, ki);
+coxReg_ki <- parSapply(clus, names(geneCV)[1:numGenes], FUN= coxReg, ki);
 coxReg_ki <- data.frame(t(data.frame(coxReg_ki)));
 colnames(coxReg_ki) <- c("Gene", "P.Value");
 
-coxReg_hn <- sapply(names(geneCV)[1:numGenes], FUN= coxReg, hn);
+coxReg_hn <- parSapply(clus, names(geneCV)[1:numGenes], FUN= coxReg, hn);
 coxReg_hn <- data.frame(t(data.frame(coxReg_hn)));
 colnames(coxReg_hn) <- c("Gene", "P.Value");
 
 
 #Gaussian Mixture Model
-gmm_ov <- sapply(names(geneCV)[1:numGenes], FUN= gmmSA, ov, tVar="TimeVar", eVar="eventVar");
+gmm_ov <- parSapply(clus, names(geneCV)[1:numGenes], FUN= gmmSA, ov, tVar="TimeVar", eVar="eventVar");
 gmm_ov <- data.frame(t(data.frame(gmm_ov)));
 colnames(gmm_ov) <- c("Gene", "P.Value");
 
-gmm_pr <- sapply(names(geneCV)[1:numGenes], FUN= gmmSA, pr, tVar="TimeVar", eVar="eventVar");
+gmm_pr <- parSapply(clus, names(geneCV)[1:numGenes], FUN= gmmSA, pr, tVar="TimeVar", eVar="eventVar");
 gmm_pr <- data.frame(t(data.frame(gmm_pr)));
 colnames(gmm_pr) <- c("Gene", "P.Value");
 
-gmm_ki <- sapply(names(geneCV)[1:numGenes], FUN= gmmSA, ki, tVar="TimeVar", eVar="eventVar");
+gmm_ki <- parSapply(clus, names(geneCV)[1:numGenes], FUN= gmmSA, ki, tVar="TimeVar", eVar="eventVar");
 gmm_ki <- data.frame(t(data.frame(gmm_ki)));
 colnames(gmm_ki) <- c("Gene", "P.Value");
 
-gmm_hn <- sapply(names(geneCV)[1:numGenes], FUN= gmmSA, hn, tVar="TimeVar", eVar="eventVar");
+gmm_hn <- parSapply(clus, names(geneCV)[1:numGenes], FUN= gmmSA, hn, tVar="TimeVar", eVar="eventVar");
 gmm_hn <- data.frame(t(data.frame(gmm_hn)));
 colnames(gmm_hn) <- c("Gene", "P.Value");
 
 
+stopCluster(clus);
 
 ##########################################
 
@@ -162,25 +165,25 @@ colnames(gmm_hn) <- c("Gene", "P.Value");
 #Merge all results together into one data frame and a matrix for convenience DONE
 
 results_ov <- cbind(kmScan_ov[3], qCut50_ov[2], qCut2575_ov[2], coxReg_ov[2], gmm_ov[2]);
-colnames(results_ov) <- c("Km.Scan.Adj.P.Value", "Qcut50.P.Value", "Qcut2575.P.Value")
+colnames(results_ov) <- c("Km.Scan.Adj.P.Value", "Qcut50.P.Value", "Qcut2575.P.Value", "Cox.P.Value", "Gmm.P.Value")
 resultsMat_ov <- sapply(results_ov, FUN=factToNum);
 rownames(resultsMat_ov) <- rownames(resultsMat_ov);
 write.table(resultsMat_ov, "allresults_ov.txt", sep="\t", row.names=T);
 
 results_pr <- cbind(kmScan_pr[3], qCut50_pr[2], qCut2575_pr[2], coxReg_pr[2], gmm_pr[2]);
-colnames(results_pr) <- c("Km.Scan.Adj.P.Value", "Qcut50.P.Value", "Qcut2575.P.Value")
+colnames(results_pr) <- c("Km.Scan.Adj.P.Value", "Qcut50.P.Value", "Qcut2575.P.Value","Cox.P.Value", "Gmm.P.Value")
 resultsMat_pr <- sapply(results_pr, FUN=factToNum);
 rownames(resultsMat_pr) <- rownames(resultsMat_pr);
 write.table(resultsMat_pr, "allresults_pr.txt", sep="\t", row.names=T);
 
 results_ki <- cbind(kmScan_ki[3], qCut50_ki[2], qCut2575_ki[2], coxReg_ki[2], gmm_ki[2]);
-colnames(results_ki) <- c("Km.Scan.Adj.P.Value", "Qcut50.P.Value", "Qcut2575.P.Value")
+colnames(results_ki) <- c("Km.Scan.Adj.P.Value", "Qcut50.P.Value", "Qcut2575.P.Value", "Cox.P.Value", "Gmm.P.Value")
 resultsMat_ki <- sapply(results_ki, FUN=factToNum);
 rownames(resultsMat_ki) <- rownames(resultsMat_ki);
 write.table(resultsMat_ki, "allresults_ki.txt", sep="\t", row.names=T);
 
 results_hn <- cbind(kmScan_hn[3], qCut50_hn[2], qCut2575_hn[2], coxReg_hn[2], gmm_hn[2]);
-colnames(results_hn) <- c("Km.Scan.Adj.P.Value", "Qcut50.P.Value", "Qcut2575.P.Value")
+colnames(results_hn) <- c("Km.Scan.Adj.P.Value", "Qcut50.P.Value", "Qcut2575.P.Value", "Cox.P.Value", "Gmm.P.Value")
 resultsMat_hn <- sapply(results_hn, FUN=factToNum);
 rownames(resultsMat_hn) <- rownames(resultsMat_hn);
 write.table(resultsMat_hn, "allresults_hn.txt", sep="\t", row.names=T);
