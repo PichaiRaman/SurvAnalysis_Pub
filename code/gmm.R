20342b612c82f4a062265b0d8fd39c0ba2a34780
PichaiRaman <pichai.raman@gmail.com>
2015-09-20 13:00:00 -0400

    Update gmm.R

--- code/gmm.R
+++ code/gmm.R
@@ -7,7 +7,9 @@ gmmSA <- function(genes, myData, createPlot=T, tVar="time", eVar="event")
     tmpMeta <- myData[[2]];
     myGene <- myData[[1]][genes,];
     tmpMeta[,"Gene"] <- as.numeric(myGene);
-
+    out <- c(genes, 1);
+    if(median(tmpMeta[,"Gene"])!=0)
+    {
     tmpMix <- normalmixEM(tmpMeta[,"Gene"], maxit=1000, maxrestarts=20);
     myOut <- data.frame(tmpMix$posterior);
     tmpMeta[,"Groups"] <- myOut[,1]> myOut[,2];
@@ -26,5 +28,6 @@ gmmSA <- function(genes, myData, createPlot=T, tVar="time", eVar="event")
     t.survfit <- survfit(t.Surv~Groups, data= tmpMeta);
     myP <- pchisq(survdiff(t.Surv~ Groups, data= tmpMeta, rho=0)$chisq, df=1, lower=F);
     out <- c(genes, myP)   
-    
+    }
+    return(out);
 }
