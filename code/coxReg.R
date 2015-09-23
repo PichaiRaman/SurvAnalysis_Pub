470a31b20b8615c597921bde3f835bf6a1ec1600
PichaiRaman <pichai.raman@gmail.com>
2015-09-23 14:21:58 -0400

    Update coxReg.R

--- code/coxReg.R
+++ code/coxReg.R
@@ -4,13 +4,13 @@
 coxReg <- function(genes, myData)
 {
     #Get metadata
-    
+    print(genes);
     tmpMeta <- myData[[2]];
     
     myGene <- myData[[1]][genes,];
     tmpMeta[,"Gene"] <- as.numeric(myGene);
     tmpMeta <- tmpMeta[order(tmpMeta[,"Gene"]),]
-
+    
     coxExpAnalysis <- coxph(formula = Surv(TimeVar, eventVar) ~ Gene, data = tmpMeta)
     pVal <- summary(coxExpAnalysis)[7][[1]][5];
     out <- c(genes, pVal);
