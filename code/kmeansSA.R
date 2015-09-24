4cae37037dedbd4b78348f99c6daab596e34ebdb
PichaiRaman <pichai.raman@gmail.com>
2015-09-24 11:59:14 -0400

    Update kmeansSA.R

--- code/kmeansSA.R
+++ code/kmeansSA.R
@@ -7,6 +7,10 @@ kmeansSA <- function(genes, myData, createPlot=T, tVar="time", eVar="event")
     tmpMeta <- myData[[2]];
     myGene <- myData[[1]][genes,];
     tmpMeta[,"Gene"] <- as.numeric(myGene);
+    temp <- table(tmpMeta[,"Gene"])
+    out <- c(genes, 1);
+    if(names(temp)[temp == max(temp)]!=0)
+    {
     set.seed(314);
     tmpMeta[,"Groups"] <- kmeans(tmpMeta[,"Gene"], centers=2)[[1]]-1
     tmpMeta <- tmpMeta[order(tmpMeta[,"Gene"]),]
@@ -23,6 +27,7 @@ kmeansSA <- function(genes, myData, createPlot=T, tVar="time", eVar="event")
     t.Surv <- Surv(timeVar, eventVar);
     t.survfit <- survfit(t.Surv~Groups, data= tmpMeta);
     myP <- pchisq(survdiff(t.Surv~ Groups, data= tmpMeta, rho=0)$chisq, df=1, lower=F);
-    out <- c(genes, myP)   
+    out <- c(genes, myP)
+    }
     return(out);
 }
