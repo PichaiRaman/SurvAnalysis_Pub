83f17ca5df8d3d69e0b8d7020f40a30a4ae1fdb3
PichaiRaman <pichai.raman@gmail.com>
2015-12-11 15:33:11 +0000

    updated code

---
+++ code/insilicoDataMethCompare.R
@@ -0,0 +1,20 @@
+##############################################
+#Code to simulate RNA-Seq data and see
+#how well methods can deal with noise 
+#in the system
+#
+##############################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
