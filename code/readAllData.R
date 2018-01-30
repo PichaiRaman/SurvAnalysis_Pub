65bdef92497f9fe5d8528a436bcbbdfaeccbd2c3
rathik <rathik@email.chop.edu>
2018-01-30 17:41:07 -0500

    updates code

--- code/readAllData.R
+++ code/readAllData.R
@@ -1,27 +1,27 @@
 ######################################
-#Code to read all data and put it in an R
-#object for further processing
-#
-#12/11/2015
-#
+# Author: Pichai Raman 
+# Function: Code to read all data and 
+# put it in an R object for 
+# further processing
+# Date: 12/11/2015
 ######################################
 
-#ovarian
+# ovarian
 annot_ov <- read.delim("../data/raw/ovca/annot.txt");
 exprs_ov <- read.delim("../data/raw/ovca/exprs.txt")
 ov <- list(exprs_ov, annot_ov);
 
-#prostate
+# prostate
 annot_pr <- read.delim("../data/raw/prca/annot.txt");
 exprs_pr <- read.delim("../data/raw/prca/exprs.txt")
 pr <- list(exprs_pr, annot_pr);
 
-#head and neck
+# head and neck
 annot_hn <- read.delim("../data/raw/hnca/annot.txt");
 exprs_hn <- read.delim("../data/raw/hnca/exprs.txt")
 hn <- list(exprs_hn, annot_hn);
 
-#Kidney
+# kidney
 annot_ki <- read.delim("../data/raw/kica/annot.txt");
 exprs_ki <- read.delim("../data/raw/kica/exprs.txt")
 ki <- list(exprs_ki, annot_ki);
