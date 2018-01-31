81fca6a9453ec7fa310b0b9440fc10a290b2797a
rathik <rathik@email.chop.edu>
2018-01-31 12:22:24 -0500

    adds code to create PCA

--- code/createPCA.R
+++ code/createPCA.R
@@ -1,56 +1,72 @@
-##############################
-# Author: Pichai Raman 
+####################################
+# Author: Pichai Raman, Komal Rathi
 # Function: Code to run PCA Analysis
 # Date: 12/3/2015
-##############################
+####################################
+
+# load libraries
+library(scatterplot3d)
 
 #########################################
-#Read in data DONE
-print("Starting Read of Data");
+# read in data DONE
+print("Starting Read of Data")
 
+# ovarian
 exprs_ov <- read.delim("../data/raw/ovca/exprs.txt")
 colnames(exprs_ov) <- gsub("\\.", "-", colnames(exprs_ov))
-rownames(exprs_ov) <- gsub(" ", "", rownames(exprs_ov));
+rownames(exprs_ov) <- gsub(" ", "", rownames(exprs_ov))
 
-#prostate
+# prostate
 exprs_pr <- read.delim("../data/raw/prca/exprs.txt")
-colnames(exprs_pr) <- gsub("\\.", "-", colnames(exprs_pr));
-rownames(exprs_pr) <- gsub(" ", "", rownames(exprs_pr));
+colnames(exprs_pr) <- gsub("\\.", "-", colnames(exprs_pr))
+rownames(exprs_pr) <- gsub(" ", "", rownames(exprs_pr))
 
-#head and neck
+# head and neck
 exprs_hn <- read.delim("../data/raw/hnca/exprs.txt")
 colnames(exprs_hn) <- gsub("\\.", "-", colnames(exprs_hn))
-rownames(exprs_hn) <- gsub(" ", "", rownames(exprs_hn));
+rownames(exprs_hn) <- gsub(" ", "", rownames(exprs_hn))
 
-#Kidney
+# kidney
 exprs_ki <- read.delim("../data/raw/kica/exprs.txt")
 colnames(exprs_ki) <- gsub("\\.", "-", colnames(exprs_ki))
-rownames(exprs_ki) <- gsub(" ", "", rownames(exprs_ki));
+rownames(exprs_ki) <- gsub(" ", "", rownames(exprs_ki))
 
-print("Finished Reading Data");
+print("Finished Reading Data")
 ##########################################
 
 #Sort all row anmes
-exprs_ov <- exprs_ov[rownames(exprs_ov),];
-exprs_pr <- exprs_pr[rownames(exprs_ov),];
-exprs_hn <- exprs_hn[rownames(exprs_ov),];
-exprs_ki <- exprs_ki[rownames(exprs_ov),];
-bigDF <- cbind(exprs_ov, exprs_pr, exprs_hn, exprs_ki);
+exprs_ov <- exprs_ov[rownames(exprs_ov),]
+exprs_pr <- exprs_pr[rownames(exprs_ov),]
+exprs_hn <- exprs_hn[rownames(exprs_ov),]
+exprs_ki <- exprs_ki[rownames(exprs_ov),]
+bigDF <- cbind(exprs_ov, exprs_pr, exprs_hn, exprs_ki)
 
 #Let's remove all rows with mean count < 100
-myRowMean <- rowMeans(bigDF);
+myRowMean <- rowMeans(bigDF)
 bigDF <- bigDF[myRowMean>100,]
-bigDF <- log2(bigDF+1);
-bigDF <- data.frame(bigDF);
-cancCol <- c(rep("OV",ncol(exprs_ov)), rep("PR",ncol(exprs_pr)), rep("HN",ncol(exprs_hn)), rep("KI",ncol(exprs_ki)));
+bigDF <- log2(bigDF+1)
+bigDF <- data.frame(bigDF)
+cancCol <- c(rep("OV",ncol(exprs_ov)), rep("PR",ncol(exprs_pr)), rep("HN",ncol(exprs_hn)), rep("KI",ncol(exprs_ki)))
 
-bigAnnot <- data.frame(colnames(bigDF), cancCol);
-outPCA <- prcomp(bigDF)$rotation;
+bigAnnot <- data.frame(colnames(bigDF), cancCol)
+outPCA <- prcomp(bigDF)$rotation
 
-write.table(outPCA, "../results/PCA.txt", sep="\t", row.names=T);
+write.table(outPCA, "../results/PCA.txt", sep="\t", row.names=T)
+write.table(bigAnnot, "../results/PCA_Annot.txt", sep="\t", row.names=T)
 
-write.table(bigAnnot, "../results/PCA_Annot.txt", sep="\t", row.names=T);
+#### merge code to create PCA plot ####
+# create plot 
+pcaData <- outPCA
+pcaData <- pcaData[1:3]
+pcaAnnot <- bigAnnot
+labs <- as.character(pcaAnnot[,2])
+labs <- gsub("OV", "#999999", labs)
+labs <- gsub("PR", "#E69F00", labs)
+labs <- gsub("HN", "#56B4E9", labs)
+labs <- gsub("KI", "#FF4500", labs)
 
-
-
+png("../Figures/PCA_Cancers.png", width=2880, height=2880,  res=324)
+scatterplot3d(pcaData[,3], pcaData[,2], pcaData[,1], xlab="PC1", ylab="PC2", zlab="PC3", color=labs,  pch=16)
+legend("top", legend=c("OV", "PR", "HN", "KI"),  col =  c("#999999", "#E69F00", "#56B4E9", "#FF4500"), inset=-.1, pch=16, xpd=T, horiz=T)
+dev.off()
 
