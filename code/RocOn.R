19634821c8c6bfed4f2f5893c550a6fc0306bc9a
rathik <rathik@email.chop.edu>
2018-02-01 17:34:31 -0500

    adds variation of Rocon

--- code/RocOn.R
+++ code/RocOn.R
@@ -22,8 +22,7 @@ require("ggplot2");
 #isDecreasing : by default the lower the metric the better, i.e. if you have a score of 1 its better than a score of 2 (i.e. p-values), if using fold change set to T
 #myTitle : title of the plot
 ############################################
-rocon <- function(data, diagCol="black", roccol="red", isDecreasing=T, myTitle="ROC Curve")
-{
+rocon <- function(data, diagCol="black", roccol="red", isDecreasing=T, myTitle="ROC Curve") {
     #Removes all NA
     data <- na.omit(data);
     data[,2] <- as.numeric(as.character(data[,2]));
@@ -52,7 +51,12 @@ rocon <- function(data, diagCol="black", roccol="red", isDecreasing=T, myTitle="
     #Create and return object
     rocdata <- data.frame(FPR = x, TPR = y, method="")
     legLabs <- paste("AUC = ", round(myAuc, 3), sep="");
-    p <- ggplot(data = rocdata, aes(x = FPR, y = TPR, color=method)) +geom_line(size=.5) + geom_line(data = diag, aes(x = x,y = y), color ="black")+theme_bw()+ labs(x = "False Positive Rate",y = "True Positive Rate", title = myTitle)+scale_color_discrete(labels=legLabs)+ theme(legend.title=element_blank())
+    p <- ggplot(data = rocdata, aes(x = FPR, y = TPR, color=method)) + 
+      geom_line(size=.5) + 
+      geom_line(data = diag, aes(x = x,y = y), color ="black") + 
+      theme_bw() + 
+      labs(x = "False Positive Rate",y = "True Positive Rate", title = myTitle) + 
+      scale_color_discrete(labels=legLabs) + theme(legend.title=element_blank())
 
 rocdata <- rocdata[1:2];
     return(list(rocdata, p, myAuc));
@@ -68,8 +72,7 @@ rocdata <- rocdata[1:2];
 #myTitle : title of the plot
 ############################################
 
-roconMult <- function(data, diagCol="black", colors=NULL, isDecreasing=T, myTitle="ROC Curve")
-{
+roconMult <- function(data, diagCol="black", colors=NULL, isDecreasing=T, myTitle="ROC Curve") {
     
     dataList <- split(data, f=data[,3]);
     diag = data.frame(x = seq(0, 1, by = .01), y = seq(0, 1, by = .01))
@@ -86,12 +89,41 @@ roconMult <- function(data, diagCol="black", colors=NULL, isDecreasing=T, myTitl
     
     #Create and return object
     
-    p <- ggplot(data = dfAll, aes(x = FPR, y = TPR, color=method)) +geom_line(size=.5) + geom_line(data = diag, aes(x = x,y = y), color ="black")+theme_bw()+ labs(x = "False Positive Rate",y = "True Positive Rate", title = myTitle)+scale_color_discrete(labels=legLabs)+ theme(legend.title=element_blank())
+    p <- ggplot(data = dfAll, aes(x = FPR, y = TPR, color=method)) +
+      geom_line(size=.5) + 
+      geom_line(data = diag, aes(x = x,y = y), color = "black") +
+      theme_bw() + 
+      labs(x = "False Positive Rate",y = "True Positive Rate", title = myTitle) + 
+      scale_color_discrete(labels=legLabs) + theme(legend.title=element_blank())
     return(list(dfAll, p, allAUC));
     
 }
 
-
+# another variation for paper
+roconMult2 <- function(data, diagCol="black", colors=NULL, isDecreasing=T, myTitle="ROC Curve") {
+  
+  dataList <- split(data, f=data[,3]);
+  diag = data.frame(x = seq(0, 1, by = .01), y = seq(0, 1, by = .01))
+  output <- lapply(dataList, FUN=rocon, isDecreasing=isDecreasing)
+  dfAll <- data.frame();
+  allAUC <- data.frame();
+  legLabs <- c();
+  for(i in 1:length(output)) {
+    dfAll <- rbind(dfAll, data.frame(output[[i]][1], method=as.character(names(output)[i])));
+    allAUC <- rbind(allAUC, data.frame(AUCValue=as.numeric(as.character(output[[i]][3])), method=names(output)[i]));
+    legLabs <- c(legLabs, paste(method=names(output)[i], " : AUC = ", round(as.numeric(as.character(output[[i]][3])), 3), sep=""));
+  }
+  
+  #Create and return object
+  
+  p <- ggplot(data = dfAll, aes(x = FPR, y = TPR, color=method)) +
+    geom_line(size=.5) + 
+    geom_line(data = diag, aes(x = x,y = y), color = "black") +
+    theme_bw() + 
+    labs(x = "False Positive Rate",y = "True Positive Rate", title = myTitle) + 
+    theme(plot.title = element_text(hjust = 0.5))
+  return(list(dfAll, p, allAUC));
+}
 
 
 
